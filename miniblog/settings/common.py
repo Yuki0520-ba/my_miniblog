6b4bbd288954d2e0933e1df8eebb5b7d82d000d2
Yuki0520-ba <>
2019-12-28 23:32:07 +0900

    static dir パス変更

--- miniblog/settings/common.py
+++ miniblog/settings/common.py
@@ -114,7 +114,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS=(
-    os.path.normpath(os.path.join(BASE_DIR, "assets" )),
+    os.path.normpath(os.path.join(BASE_DIR, "static" )),
 )
 
 LOGIN_REDIRECT_URL ='/'
