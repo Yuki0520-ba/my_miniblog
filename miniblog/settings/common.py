704bfc1d1ec53285397352ebf28d9e9b7d5fc6a1
Yuki0520-ba <>
2019-12-28 23:35:27 +0900

    no message

--- miniblog/settings/common.py
+++ miniblog/settings/common.py
@@ -114,7 +114,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS=(
-    os.path.normpath(os.path.join(BASE_DIR, "static" )),
+    os.path.normpath(os.path.join(BASE_DIR, "assets" )),
 )
 
 LOGIN_REDIRECT_URL ='/'
